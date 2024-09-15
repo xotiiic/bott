d8c093cf5511dea13e23fc8e02acda4ed32eb9d0
xotiiic <143630859+xotiiic@users.noreply.github.com>
2024-09-15 21:36:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
   ownerID: ["1000489107379720262", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
+  embedColor: "#16191e",
   musicardTheme:"themes16", //Goes from themes1 to themes19
   activityName: "You", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
